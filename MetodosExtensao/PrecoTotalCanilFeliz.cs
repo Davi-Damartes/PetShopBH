c3902ad08206b31a1ceb948bf03c6cbf0b933248
Davi Lima <davidamartesgit@Gmail.com>
2024-08-02 16:40:21 -0300

    Método para caluclar preço do Canino Feliz

---
+++ MetodosExtensao/PrecoTotalCanilFeliz.cs
@@ -0,0 +1,29 @@
+﻿using PetShopBH.Models;
+
+namespace PetShopBH.MetodosExtensao
+{
+    public static class PrecoTotalCanilFeliz
+    {
+        public static decimal CalcularTotalSemanaCaninoFeliz(this PetShopFeliz petshop, DonoCachorros donoCachorros)
+        {
+            decimal somaCachorroG = donoCachorros.QuantidadeG * petshop.PrecoBanhoG;
+            decimal somaCachorroP = donoCachorros.QuantidadeP * petshop.PrecoBanhoP;
+
+            return somaCachorroG + somaCachorroP;
+
+        }
+        public static decimal CalcularTotalFinalDeSemanaFeliz(this PetShopFeliz petshop, DonoCachorros donoCachorros)
+        {
+            decimal precoPorcentagemG = 40m * 1.20m;
+            decimal precoPorcentagemP = 20m * 1.20m;
+
+            petshop.PrecoBanhoP = precoPorcentagemP;
+            petshop.PrecoBanhoG = precoPorcentagemG;
+
+            decimal somaCachorroG = donoCachorros.QuantidadeG * petshop.PrecoBanhoG;
+            decimal somaCachorroP = donoCachorros.QuantidadeP * petshop.PrecoBanhoP;
+            return somaCachorroG + somaCachorroP;
+
+        }
+    }
+}
