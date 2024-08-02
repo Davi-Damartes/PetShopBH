c45ef339f9f1e60ee88815d57115d60161b7973b
Davi Lima <davidamartesgit@Gmail.com>
2024-08-02 13:55:31 -0300

    Método para calcular o preço do PetShopChowChow

---
+++ MetodosExtensao/PrecoTotalChowChow.cs
@@ -0,0 +1,16 @@
+﻿using PetShopBH.Models;
+
+namespace PetShopBH.MetodosExtensao
+{
+    public static class TotalChowChow
+    {
+        public static decimal CalcularTotalChowChow(this PetShopChowChow petshop, DonoCachorros donoCachorros)
+        {
+            decimal somaCachorroG = donoCachorros.QuantidadeG * petshop.PrecoBanhoG;
+            decimal somaCachorroP = donoCachorros.QuantidadeP * petshop.PrecoBanhoP;
+
+            return somaCachorroG + somaCachorroP;
+
+        }
+    }
+}
