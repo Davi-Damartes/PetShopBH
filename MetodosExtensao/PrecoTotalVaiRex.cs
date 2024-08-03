b486aaf21537a7ab40ac7b6e0fdf3153ae89933b
Davi Lima <davidamartesgit@Gmail.com>
2024-08-02 22:39:25 -0300

    Adiciona classe com métodos para calcular preço

---
+++ MetodosExtensao/PrecoTotalVaiRex.cs
@@ -0,0 +1,29 @@
+﻿using PetShopBH.Models;
+
+namespace PetShopBH.MetodosExtensao
+{
+    public static class PrecoTotalVaiRex
+    {
+        public static decimal CalcularTotalSemanaVaiRex(this PetShopRex petshop, DonoCachorros donoCachorros)
+        {
+            petshop.PrecoBanhoDogPequeno = 15.00M;
+            petshop.PrecoBanhoDogGrande = 50.00M;
+            decimal somaCachorroG = donoCachorros.QuantidadeDogGrande * petshop.PrecoBanhoDogGrande;
+            decimal somaCachorroP = donoCachorros.QuantidadeDogPequeno * petshop.PrecoBanhoDogPequeno;
+
+            return somaCachorroG + somaCachorroP;
+
+        }
+        public static decimal CalcularTotalFinalSemanaVaiRex(this PetShopRex petshop, DonoCachorros donoCachorros)
+        {
+            petshop.PrecoBanhoDogGrande = 20.00M;
+            petshop.PrecoBanhoDogGrande = 55.00M;
+            decimal somaCachorroG = donoCachorros.QuantidadeDogGrande * petshop.PrecoBanhoDogGrande;
+            decimal somaCachorroP = donoCachorros.QuantidadeDogPequeno * petshop.PrecoBanhoDogPequeno;
+
+            return somaCachorroG + somaCachorroP;
+
+        }
+
+    }
+}
